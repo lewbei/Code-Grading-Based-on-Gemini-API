c198f211b7a19c1b8d15395c102086b58fdbc2d9
lewbei <33484590+lewbei@users.noreply.github.com>
2025-02-27 16:37:01 +0800

    Update and rename check gemini model to sc/check gemini model.py

--- sc/check gemini model.py
+++ sc/check gemini model.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(r'your/path/.env') #need to ensure you have '.env'
 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 
 # List available models
